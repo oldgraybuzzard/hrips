8bd578929e259d805441ece6d1597c234d8aa0e9
Kendall Felder <61666382+oldgraybuzzard@users.noreply.github.com>
2021-11-24 21:22:59 -0500

    employee model creation

---
+++ models/Employee.js
@@ -0,0 +1,60 @@
+const { Model, DataTypes } = require('sequelize');
+const sequelize = require('../config/connection');
+
+class Employee extends Model {}
+
+Comment.init(
+  {
+    id: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      primaryKey: true,
+      autoIncrement: true
+    },
+    first_name: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      validate: {
+        len: [1],
+        max: 30
+      }
+    },
+    last_name: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      validate: {
+        len: [1],
+        max: 30
+      }
+    },
+    email: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      validate: {
+        isEmail: true
+      }
+    },
+    manager_id: {
+      type: DataTypes.INTEGER,
+      references: {
+        model: 'manager',
+        key: 'id'
+      }
+    },
+    role_id: {
+      type: DataTypes.INTEGER,
+      references: {
+        model: 'role',
+        key: 'id'
+      }
+    }
+  },
+  {
+    sequelize,
+    freezeTableName: true,
+    underscored: true,
+    modelName: 'employee'
+  }
+);
+
+module.exports = Employee;
---
+++ models/index.js
@@ -0,0 +1,14 @@
+//Imports models into this area
+const Employee = require('./Employee');
+const Manager = require('./Manager');
+const Role = require('./Role');
+
+//Associations created in the area below
+
+
+
+
+
+
+
+module.exports = { Employee, Manager, Role };
