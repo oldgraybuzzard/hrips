e9a68b256b4b512db06538d150d54627b6313627
Kendall Felder <61666382+oldgraybuzzard@users.noreply.github.com>
2021-11-25 06:14:17 -0500

    add in associaitions

--- models/index.js
+++ models/index.js
@@ -4,11 +4,21 @@ const Department = require('./Department');
 const Role = require('./Role');
 
 //Associations created in the area below
-
-
-
-
-
-
+Role.belongsTomany(Employee, {
+  foreignKey: 'role_id',
+  onDelete: 'SET NULL'
+}),
+Role.hasMany(Employee, {
+  foreignKey: 'employee_id',
+  onDelete: 'SET NULL'
+}),
+Department.belongsToMany(Employee, {
+  foreignKey: 'department_id',
+  onDelete: 'SET NULL'
+}),
+Employee.belongsTo(Department,{
+  foreignKey: 'department_id',
+  onDelete: 'SET NULL'
+}),
 
 module.exports = { Employee, Department, Role };
