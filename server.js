d39bc7b004c293a40b7183310710c77d57579660
Kendall Felder <61666382+oldgraybuzzard@users.noreply.github.com>
2021-11-29 20:14:56 -0500

    added pdf-creator-node and pdf template in partials file

--- server.js
+++ server.js
@@ -1,11 +1,18 @@
 const express = require('express');
 const db = require('./config/connection');
+// Required for PDF
+const pdf = require('pdf-creator-node');
+
+const fs = require('fs');
 //api route for future development
 const apiRoutes = require('./controllers');
 
 
 const PORT = process.env.PORT || 3001;
 const app = express();
+
+//for pdf
+const html = fs.readFileSync('./views/partials/pdfTemplate.handlebars')
 
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
