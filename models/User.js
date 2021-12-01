a8ff8af8778baa1d5891806718a42b2554d53c0b
Kendall Felder <61666382+oldgraybuzzard@users.noreply.github.com>
2021-11-30 21:49:56 -0500

    update security features

--- models/User.js
+++ models/User.js
@@ -1,8 +1,15 @@
 const { Model, DataTypes } = require('sequelize');
+const bcrypt = require('bcrypt');
 const sequelize = require('../config/connection');
 
+
 // create our User model
-class User extends Model {}
+class User extends Model {
+
+checkPassword(LoginPW) {
+  return bcrypt.compareSync(LoginPW, this.password);
+  }
+}
 
 // define table columns and configuration
 User.init(
@@ -34,6 +41,18 @@ User.init(
     }
   },
   {
+    hooks: {
+      // set up beforeCreate lifecycle "hook" functionality
+      async beforeCreate(newUserData) {
+        newUserData.password = await bcrypt.hash(newUserData.password, 10);
+        return newUserData;
+      },
+
+      async beforeUpdate(updatedUserData) {
+        updatedUserData.password = await bcrypt.hash(updatedUserData.password, 10);
+        return updatedUserData;
+      }
+    },
     sequelize,
     timestamps: false,
     freezeTableName: true,
